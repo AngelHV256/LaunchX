26bf1199474be0737ce5182fa6553e8d3d1e8ffc
AngelHV256 <angelh.5057@gmail.com>
2023-01-02 19:18:50 -0600

    Agregando la seccion de estudio

--- Portafolio/JS/main.js
+++ Portafolio/JS/main.js
@@ -1,14 +1,14 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 /* Show menu*/
-if(navToggle) {
+if (navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu');
   })
 }
 /** Hidden menu**/
-if(navClose) {
+if (navClose) {
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
   })
@@ -24,19 +24,38 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /* Skills List */
 const skillsContent = document.getElementsByClassName('skills_content'),
-      skillsHeader = document.querySelectorAll('.skills_header');
+  skillsHeader = document.querySelectorAll('.skills_header');
 
-function toggleSkills(){
+function toggleSkills() {
   let itemClass = this.parentNode.className;
 
-  for(i = 0; i < skillsContent.length; i++){
+  for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = 'skills_content skills_close';
   }
-  if(itemClass === 'skills_content skills_close'){
+  if (itemClass === 'skills_content skills_close') {
     this.parentNode.className = 'skills_content skills_open';
   }
 }
 
 skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills);
+});
+
+/* Qualification Tabs */
+
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data_content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification_active');
+    });
+    target.classList.add('qualification_active');
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification_active');
+    })
+    tab.classList.add('qualification_active');
+  });
 });
